65741044c14c1d1c42530b3e9f05be8278327b4e
CEO <wainainaken@gmail.com>
2016-12-03 11:19:31 +0300

    Update list.py

--- list.py
+++ list.py
@@ -1,16 +1,16 @@
 """MaxMin application"""
 def minMax(numbersList):
 
-    outputlist=[]
+    outputlist=[]#empty list to store max and min values
 
     temp_max=max(numbersList)
     temp_min=min(numbersList)
 
-    if temp_max is temp_min:
+    if temp_max is temp_min: #if max and min values are equal,store one of them in outputlist.
        outputlist.append(temp_max)
        print "all numbers equal:",   sorted(outputlist)
 
-    else:
+    else: #store of max and min values in outputlist if they are not equal.
          outputlist.append(temp_min)
          outputlist.append(temp_max)
          print "minimum & maximum numbers respectively:",   sorted(outputlist)
